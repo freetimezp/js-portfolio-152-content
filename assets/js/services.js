24e192e314ea518a30e7198ce4ec5a653ac6776b
freetimezp <kraftukrnet@ukr.net>
2024-11-15 23:39:54 +0200

    03. js-portfolio-152-content. GSAP animation.

---
+++ assets/js/services.js
@@ -0,0 +1,50 @@
+export const servicesCopy = [
+    [
+        `Lorem 1, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, 
+        perferendis. Totam earum fuga omnis iste pariatur eius quos accusantium, 
+        blanditiis voluptatum, possimus obcaecati accusamus, veritatis neque 
+        provident molestiae. Inventore porro ex explicabo culpa quo velit.`,
+    ],
+    [
+        `Lorem 2, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, 
+        perferendis. Totam earum fuga omnis iste pariatur eius quos accusantium, 
+        blanditiis voluptatum, possimus obcaecati accusamus, veritatis neque 
+        provident molestiae. Inventore porro ex explicabo culpa quo velit.`,
+    ],
+    [
+        `Lorem 3, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, 
+        perferendis. Totam earum fuga omnis iste pariatur eius quos accusantium, 
+        blanditiis voluptatum, possimus obcaecati accusamus, veritatis neque 
+        provident molestiae. Inventore porro ex explicabo culpa quo velit.`,
+    ],
+    [
+        `Lorem 4, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, 
+        perferendis. Totam earum fuga omnis iste pariatur eius quos accusantium, 
+        blanditiis voluptatum, possimus obcaecati accusamus, veritatis neque 
+        provident molestiae. Inventore porro ex explicabo culpa quo velit.`,
+    ],
+    [
+        `Lorem 5, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, 
+        perferendis. Totam earum fuga omnis iste pariatur eius quos accusantium, 
+        blanditiis voluptatum, possimus obcaecati accusamus, veritatis neque 
+        provident molestiae. Inventore porro ex explicabo culpa quo velit.`,
+    ],
+    [
+        `Lorem 6, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, 
+        perferendis. Totam earum fuga omnis iste pariatur eius quos accusantium, 
+        blanditiis voluptatum, possimus obcaecati accusamus, veritatis neque 
+        provident molestiae. Inventore porro ex explicabo culpa quo velit.`,
+    ],
+    [
+        `Lorem 7, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, 
+        perferendis. Totam earum fuga omnis iste pariatur eius quos accusantium, 
+        blanditiis voluptatum, possimus obcaecati accusamus, veritatis neque 
+        provident molestiae. Inventore porro ex explicabo culpa quo velit.`,
+    ],
+    [
+        `Lorem 8, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, 
+        perferendis. Totam earum fuga omnis iste pariatur eius quos accusantium, 
+        blanditiis voluptatum, possimus obcaecati accusamus, veritatis neque 
+        provident molestiae. Inventore porro ex explicabo culpa quo velit.`,
+    ],
+];
